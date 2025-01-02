8d9d448694f69a4206f887ae7ee6cc257e7d8b52
efar716 <110000165+efar716@users.noreply.github.com>
2025-01-02 15:23:01 +1300

    landing page styling

--- src/pages/LandingPanel.jsx
+++ src/pages/LandingPanel.jsx
@@ -10,10 +10,25 @@ const LandingPanel = () => {
             <section className="table-of-contents">
                 <h1>Table of Content</h1>
                 <nav id="table-content-grid">
-                    <div className="table-of-contents-grid"> <a href="#about-me">#1 About Me</a></div>
-                    <div className="table-of-contents-grid"><a href="#projects">#2 Projects</a></div>
-                    <div className="table-of-contents-grid"><a href="#blog">#3 Blog</a></div>
-                    <div className="table-of-contents-grid"><a href="#connect">#4 Connect</a></div>
+                    <div className="table-of-contents-grid">
+                        <a href="#about-me">#1 About Me</a>
+                        <img src="src/assets/logo.svg" />
+                    </div>
+
+                    <div className="table-of-contents-grid">
+                        <a href="#projects">#2 Projects</a>
+                        <img src="src/assets/projects.svg" />
+
+                    </div>
+                    <div className="table-of-contents-grid">
+                        <a href="#blog">#3 Blog</a>
+                        <img src="src/assets/blogs.svg" />
+                    </div>
+                    <div className="table-of-contents-grid">
+                        <a href="#connect">#4 Connect</a>
+                        <img src="src/assets/socials.svg" />
+
+                    </div>
 
 
 
